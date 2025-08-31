2c17b8814c617a70f44ec3d6d4177fa41972f8cd
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 15:59:50 +0100

    Update publicPath to be more flexible for deployment environments

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/assets/' 
+    publicPath: process.env.PUBLIC_URL || '/assets/' 
   },
   module: {
     rules: [
