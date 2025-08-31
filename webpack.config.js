712305441deee7907eafc71fd50bbb7d7d408661
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 15:58:58 +0100

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/' 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  
+        exclude: /node_modules/,  
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,  
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
