73325fc15dccea5830b513a21af15c3e204e7ccc
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 15:59:01 +0100

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const App = () => {
+  return <h1>Welcome to the Modern Design System</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
